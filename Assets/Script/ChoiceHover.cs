170da2f6700a57a1a11c56c2105c30f3bf5d11bd
陽 <youwanashityu@gmail.com>
2026-01-24 17:52:47 +0900

    [update]選択肢にカーソルを合わせた際横にマークが表示され、指定したカラーになるようにしました

--- Assets/Script/ChoiceHover.cs
+++ Assets/Script/ChoiceHover.cs
@@ -1,45 +1,75 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 using UnityEngine.UI;
 
 public class ChoiceHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [Header("ホバー時に表示するUI（任意）")]
-    public GameObject hoverUI;
+    [Header("ホバー時の文字色")]
+    public Color hoverTextColor = Color.yellow;
 
-    [Header("ホバー時の色")]
-    public Color hoverColor = new Color(1f, 0.9f, 0.6f);
+    [Header("ホバー時のUI色")]
+    public Color hoverUIColor = Color.yellow;
 
-    private Button button;
-    private Color normalColor;
+    [Header("ホバー時に表示するUI")]
+    public RectTransform hoverUI;
+
+    [Header("UI の表示オフセット")]
+    public Vector2 offset = new Vector2(-200f, 0f);
+
+    private TextMeshProUGUI textMesh;
+    private Color normalTextColor;
+
+    private Image hoverUIImage;
+    private Color normalUIColor;
+
+    private RectTransform buttonRect;
 
     void Start()
     {
-        button = GetComponent<Button>();
-        normalColor = button.colors.normalColor;
+        textMesh = GetComponentInChildren<TextMeshProUGUI>();
+        normalTextColor = textMesh.color;
+
+        buttonRect = GetComponent<RectTransform>();
+
+        if (hoverUI != null)
+        {
+            hoverUIImage = hoverUI.GetComponent<Image>();
+            if (hoverUIImage != null)
+                normalUIColor = hoverUIImage.color;
+
+            hoverUI.gameObject.SetActive(false);
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // UI を表示
-        if (hoverUI != null)
-            hoverUI.SetActive(true);
+        textMesh.color = hoverTextColor;
 
-        // 色変更
-        var cb = button.colors;
-        cb.normalColor = hoverColor;
-        button.colors = cb;
+        if (hoverUI != null)
+        {
+            hoverUI.gameObject.SetActive(true);
+
+            // UI の位置をボタンの右側に移動
+            hoverUI.anchoredPosition = buttonRect.anchoredPosition + offset;
+
+            // UI の色も変更
+            if (hoverUIImage != null)
+                hoverUIImage.color = hoverUIColor;
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // UI を非表示
-        if (hoverUI != null)
-            hoverUI.SetActive(false);
+        textMesh.color = normalTextColor;
 
-        // 色を元に戻す
-        var cb = button.colors;
-        cb.normalColor = normalColor;
-        button.colors = cb;
+        if (hoverUI != null)
+        {
+            hoverUI.gameObject.SetActive(false);
+
+            // UI の色を元に戻す
+            if (hoverUIImage != null)
+                hoverUIImage.color = normalUIColor;
+        }
     }
 }
